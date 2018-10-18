82df6b071d704ea17ffdefd393f7f556d7c68aa9
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-10-18 18:16:02 +0100

    Added query string logic for nh_sens_id.

--- controllers/sensors.js
+++ controllers/sensors.js
@@ -5,7 +5,12 @@ async function get(req, res, next) {
     const context = {};
 
     context.id = parseInt(req.params.id, 10);
-    context.nh_sens_id = parseInt(req.query.nh_sens_id, 10);
+
+    if (req.query.nh_sens_id === 'null') {
+      context.nh_sens_id = null;
+    } else {
+      context.nh_sens_id = parseInt(req.query.nh_sens_id, 10);
+    }
 
     const rows = await sensors.find(context);
 
