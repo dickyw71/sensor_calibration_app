925e6e3dea90bc6a7009c0527a84cdf1f6cd067c
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-11-11 18:47:48 +0000

    Corrected base query and find function bind varibles.

--- db_apis/calibrations.js
+++ db_apis/calibrations.js
@@ -1,39 +1,71 @@
 const database = require('../services/database.js');
 
-const baseQuery =
- `select sensor_part_id "sensor_part_id",
-    sensor_type_cd "sensor_type_cd",
-    sensor_part_name "sensor_part_name",
-    equip_desc "equip_desc",
-    oem_part_no "oem_part_no",
-    obsolete_flag "obsolete_flag",
-    creation_dt "creation_dt",
-    revision_dt "revision_dt",
-    revision_user "revision_user",
-    unit_cd_in "unit_cd_in",
-    unit_cd_out "unit_cd_out",
-    xy_raw_data_flag "xy_raw_data_flag",
-    type_to_use "type_to_use",
-    default_table_id "default_table_id",
-    cal_period "cal_period",
-    shelf_life "shelf_life",
-    equip_range "equip_range",
-    equip_accuracy "equip_accuracy",
-    cal_uncert "cal_uncert",
-    cal_type "cal_type",
-    ccp_ref "ccp_ref",
-    lcp_ref "lcp_ref",
-    cal_category_cd "cal_category_cd"
-  from sensor_part_definition`;
+const basequery =
+ `select sensor_cal_id "sensor_cal_id",
+    sensor_id "sensor_id",
+    sensor_cal_notes "sensor_cal_notes",
+    most_recent_cal_flag "most_recent_cal_flag",
+    legacy_table_type_id "legacy_table_type_id",
+    legacy_mod_date "legacy_mod_date",  
+    legacy_cal_date "legacy_cal_date",				    
+    legacy_exp_date "legacy_exp_date",				  
+    accepted_by "accepted_by",					 
+    cal_date "cal_date",					
+    adjusted "adjusted",				
+    adjustments "adjustments",			
+    calibrated_by "calibrated_by",					  
+    error_rep_no "error_rep_no",					 
+    extension_period "extension_period",				
+    cal_extension "cal_extension",				
+    concession_no "concession_no",			
+    cert_no "cert_no",		
+    limit_use "limit_use",	
+    temp_degc "temp_degc",
+    rel_humidity "rel_humidity",	
+    cal_final "cal_final",			
+    prev_cert_no "prev_cert_no",			
+    quarantined "quarantined",			
+    accepted_date "accepted_date",			
+    cal_authority "cal_authority",		
+    cal_period "cal_period",		
+    file_format "file_format",	
+    file_format_rev "file_format_rev",	
+    file_name "file_name",		
+    shelf_life "shelf_life",		
+    error_rep "error_rep",		
+    circulation "circulation",		
+    equip_range "equip_range",		
+    equip_accuracy "equip_accuracy", 
+    cal_uncert "cal_uncert",			
+    cal_type "cal_type",			
+    ccp_ref "ccp_ref",			
+    lcp_ref "lcp_ref",		
+    cal_status_cd "cal_status_cd",	
+    intended_use "intended_use",	
+    cal_category_cd "cal_category_cd",
+    obsolete_flag "obsolete_flag",	
+    creation_dt "creation_dt",	
+    revision_dt "revision_dt",	
+    revision_user "revision_user",	
+    customer "customer",
+    location "location",
+    misc1 "misc1",	
+    misc2 "misc2",
+    misc3 "misc3",	
+    misc4 "misc4",	
+    misc5 "misc5",	
+    misc6 "misc6"	
+  from sensor_calibration
+  where 1 = 1`;
 
 async function find(context) {
-  let query = baseQuery;
+  let query = basequery;
   const binds = {};
 
-  if (context.code) {
-    binds.sensor_part_id = context.code;
+  if (context.id) {
+    binds.sensor_cal_id = context.id;
  
-    query += `\nwhere sensor_part_id = :sensor_part_id`;
+    query += `\nwhere sensor_cal_id = :sensor_cal_id`;
   }
 
   const result = await database.simpleExecute(query, binds);
