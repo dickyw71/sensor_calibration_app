306197de92d24cc6d97921968745e870cd383ae5
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-11-14 21:50:45 +0000

    Added parts_sensors URI endpoint routing.

--- services/router.js
+++ services/router.js
@@ -6,6 +6,7 @@ const parts = require('../controllers/parts.js');
 const calibrations = require('../controllers/calibrations.js');
 const types_sensors = require('../controllers/types_sensors.js');
 const types_parts = require('../controllers/types_parts.js');
+const parts_sensors = require('../controllers/parts_sensors.js');
 const sensors_calibrations = require('../controllers/sensors_calibrations.js');
 
 router.route('/sensors/:id?')
@@ -30,5 +31,8 @@ router.route('/types/:code/parts/:id?')
 
 router.route('/sensors/:sensor_id/calibrations/:cal_id?')
   .get(sensors_calibrations.get);
+
+router.route('/parts/:part_id/sensors/:sensor_id?')
+  .get(parts_sensors.get);
 
 module.exports = router;
